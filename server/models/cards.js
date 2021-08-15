c1822aab52d7d34320a85a3779dfcab6f180fb92
huh-GitHub <f20202045@goa.bits-pilani.ac.in>
2021-08-15 11:27:00 +0530

    added a create card endpoint

--- server/models/cards.js
+++ server/models/cards.js
@@ -6,14 +6,14 @@ const cardSchema = new Schema({
   userID: { type: String, required: true },
   title: { type: String, required: true },
   description: { type: String, required: false },
-  items: [
+  items: 
     {
       title: { type: String, required: true },
       description: { type: String, required: true },
       image: { type: String, required: false },
       link: { type: String, required: true },
-    },
-  ],
+    }
+  
 });
 
 module.exports = mongoose.model('data', cardSchema, 'data');
--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
-const card = require("./schema");
+const card = require("./models/cards");
 
 router.get("/rohit", async (req, res) => {
   try {
@@ -13,10 +13,26 @@ router.get("/rohit", async (req, res) => {
     console.error(err.message);
   }
 });
+router.post('/createPost', async (req,res)=>{
+  try{
+      const newEntry = new card();
+      newEntry.userID = req.body.userID;
+      newEntry.title = req.body.title;
+      newEntry.description = req.body.description;
+      newEntry.items.title = req.body.items.title;
+      newEntry.items.description = req.body.items.description;
+      newEntry.items.img = req.body.items.img;
+      newEntry.items.link = req.body.items.link
+      newEntry.save()
+      res.send('data inserted')
+        
+      
+  }
+  catch(e){
+    console.log(e)
+  }
 
-// router.use('/' , (req, res, next) => {
-//     res.send('hello rohit');
-// });
+})
 
 
 module.exports = router;
